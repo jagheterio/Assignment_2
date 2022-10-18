6da4f95f46c221309bf758dc03f846de68115bc2
ikueohira <ikue.ohira@gmail.com>
2022-10-18 13:09:42 +0200

    Contact class

---
+++ Assignment_2/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2.Models
+{
+    internal class Contact
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+    }
+}
